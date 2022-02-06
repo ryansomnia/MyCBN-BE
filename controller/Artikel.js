e51916b030d97e6daab82fcc66f7d358aff1ea79
ryansomnia <laskarimmanuel@gmail.com>
2022-02-06 18:04:53 +0700

    try base 64

--- controller/Artikel.js
+++ controller/Artikel.js
@@ -4,13 +4,13 @@ const nodemailer = require('nodemailer');
 let dotenv = require('dotenv');
 let env = dotenv.config();
 const moment = require('moment');
-const multer = require('multer');
+// const multer = require('multer');
 
 const fs = require('fs') 
 const {promisify} = require('util')
 const writeFile = promisify(fs.writeFile);
 const secretKey = process.env.SECRET_KEY;
-const folder = process.env.DIREKTORI_IMG_DEV;
+const folder = process.env.DIREKTORI_IMG;
 const Cryptr = require('cryptr')
 const cryptr = new Cryptr(secretKey);
 
@@ -106,20 +106,21 @@ let artikel = {
 
                 let image = req.body.image
                 console.log(image);
-                // let splitbs64 = image.split(",") 
-                // let splity = image.split("/")
-                // let splitformat = splity[1].split(";")
-                // let content = splitbs64[1];
-                // console.log(content)
-                // let ext_file = splitformat[0]
-                // console.log(ext_file)
-                // let encryptedString = cryptr.encrypt(judulArtikel);
-                // let direk = `${folder}${encryptedString}.${ext_file}`;
-                // let direkString = direk.toString()
+                let splitbs64 = image.split(",") 
+                let splity = image.split("/")
+                let splitformat = splity[1].split(";")
 
-                // await writeFile(direk, content, "base64");
+                let content = splitbs64[1];
+                console.log(content)
+                let ext_file = splitformat[0]
+                console.log(ext_file)
+                let encryptedString = cryptr.encrypt(judulArtikel);
+                let direk = `${folder}${encryptedString}.${ext_file}`;
+                let direkString = direk.toString()
+
+                await writeFile(direk, content, "base64");
   
-                let qry = `INSERT INTO artikel (judulArtikel, isiArtikel, kategori, tag, waktuPembuatan, image) VALUES('${judulArtikel}', '${isiArtikel}', '${kategori}', '${tag}', '${waktuPembuatan}', '${image}')`;
+                let qry = `INSERT INTO artikel (judulArtikel, isiArtikel, kategori, tag, waktuPembuatan, image) VALUES('${judulArtikel}', '${isiArtikel}', '${kategori}', '${tag}', '${waktuPembuatan}', '${direkString}')`;
                 let hasil = await connection.execQry(qry)
                 let response = {
                     code: 200,
