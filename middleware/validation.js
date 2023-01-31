509f4b556cc6d325836dcd3794aa67ea88ac6f51
70001511952020 <heriyanto.sitorus@brilife.co.id>
2023-01-31 17:07:06 +0700

    b

--- middleware/validation.js
+++ middleware/validation.js
@@ -5,17 +5,21 @@ function validation() {
     return function (req, rest, next) {
         let tokenWithBearer = req.headers.authorization;
         if (tokenWithBearer) {
+            console.log('masuk');
             let token = tokenWithBearer.split(' ')[1];
             //verikasi
             jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
                 if (err) {
+                    console.log('err');
                     return rest.status(401).send({ auth: false, message: "Token tidak terdaftar !" });
                 } else {
+                    console.log('sukses');
                         req.auth = decoded;
                         next();
                 }
             });
         } else {
+            console.log('gagal');
             return rest.status(401).send({ auth: false, message: "Token tidak terdaftar !" });
         }
 
